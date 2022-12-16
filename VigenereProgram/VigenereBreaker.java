69a984aaaf98d311646efac655ebf0d1fa51f0d1
Ankit <ankitbhandariiitr@gmail.com>
2022-12-16 03:44:48 -0500

    Vigenere Cipher Implementation
    considering languages other than English

--- VigenereProgram/VigenereBreaker.java
+++ VigenereProgram/VigenereBreaker.java
@@ -50,8 +50,8 @@ public class VigenereBreaker {
         int klen=0;
         String message= null;
         for(int kl=1; kl<=100;kl++){
-        
-        VigenereCipher ciphe = new VigenereCipher(tryKeyLength(encrypted, kl,'e'));
+        char cha= mostCommonCharIn(dictionary);
+        VigenereCipher ciphe = new VigenereCipher(tryKeyLength(encrypted, kl,cha));
         int count= countWords(ciphe.decrypt(encrypted), dictionary);
         if(count >= lrgcnt){
             lrgcnt= count;
@@ -63,10 +63,57 @@ public class VigenereBreaker {
         System.out.println("key length is "+ klen);
         System.out.println("total valid words are"+ lrgcnt);
         return message;
-    
-    
     }
       
+    public char mostCommonCharIn(HashSet<String> dictionary){
+        String english= "abcdefghijklmnopqrstuvwxyz";
+        int [] counts= new int[english.length()];
+        for (String str: dictionary){
+            for (int i=0; i<str.length(); i++){
+                if(english.indexOf(str.charAt(i)) != -1){
+                counts[english.indexOf(str.charAt(i))]++;
+                }
+            }
+            
+        }
+        
+        return english.charAt(maxDex(counts));
+        
+    
+    }
+    
+    public int maxDex(int[] count){
+        int max=0;
+        int ind=0;
+        for(int i=0; i< count.length ;i++){
+        if(count[i]>max){
+            max = count[i];
+            ind= i;
+        }
+        
+        }
+        return ind;
+    
+    }
+    public int[] countChar(String str){
+        int [] counts= new int[26];
+        String english= "abcdefghijklmnopqrstuvwxyz";
+        for(int i=0; i<str.length(); i++){
+            if(english.indexOf(str.charAt(i)) != -1){
+                counts[english.indexOf(str.charAt(i))]++;
+            }
+        
+        }
+        return counts;
+    }
+    
+    public void breakForAllLangs(String encrypted,HashMap<String, HashSet<String>> languages){
+        for(String str:languages.keySet()){
+            String message = breakForLanguage(encrypted, languages.get(str));
+            System.out.println("*******Decrypting in *****"+ str + " language, here it is***********");
+            System.out.println(message);                  
+        }    
+    }
     
     public void breakVigenere () {
         FileResource resource = new FileResource();
@@ -81,8 +128,27 @@ public class VigenereBreaker {
         FileResource resource = new FileResource();
         String enmessage= resource.asString();
         FileResource fr = new FileResource();      
-        String message= breakForLanguage(enmessage, readDictionary(fr));
-        System.out.println("here is the decrypted message: ");
+        String message = breakForLanguage(enmessage, readDictionary(fr));
+        System.out.println(" here is the decrypted message: ");
         System.out.println(message);
+    }
+    
+    public void breakVigenere3 (){
+        FileResource resource = new FileResource();
+        String enmessage= resource.asString();
+        String[] str= {"Danish","Dutch","English", "French", "German", "Italian", "Portuguese", "Spanish"};
+        HashMap<String, HashSet<String>> languages = new HashMap<String, HashSet<String>> ();
+        for(int i=0; i< str.length; i++){
+            System.out.println("Select the file for " + str[i] + " language");
+            FileResource fr = new FileResource();            
+            languages.put(str[i],readDictionary(fr));
+            System.out.println(str[i] + " language added to HashMap");
+        }
+        
+        breakForAllLangs(enmessage,languages);
+        
+        
+
+    
     }
 }
