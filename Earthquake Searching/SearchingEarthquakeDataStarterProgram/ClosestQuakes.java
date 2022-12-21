fd0cbe9e8dae1a7845d41818ebf48e1b79199ae4
Ankit <ankitbhandariiitr@gmail.com>
2022-12-21 00:30:55 -0500

    FindingLargestEQs

--- Earthquake Searching/SearchingEarthquakeDataStarterProgram/ClosestQuakes.java
+++ Earthquake Searching/SearchingEarthquakeDataStarterProgram/ClosestQuakes.java
@@ -11,21 +11,36 @@ import java.util.*;
 public class ClosestQuakes {
     public ArrayList<QuakeEntry> getClosest(ArrayList<QuakeEntry> quakeData, Location current, int howMany) {
         ArrayList<QuakeEntry> ret = new ArrayList<QuakeEntry>();
+        //ArrayList<QuakeEntry> copy = new ArrayList<QuakeEntry>(quakeData);
         // TO DO
-
+        double min= 1000000000000.0;
+        QuakeEntry quake= null; 
+        for(int i=0; i<howMany; i++){
+            for (QuakeEntry qe : quakeData) {
+                if(current.distanceTo(qe.getLocation())< min){
+                    min = current.distanceTo(qe.getLocation());
+                    quake = qe;
+                }
+            }
+            ret.add(quake);
+            quakeData.remove(quake);
+            min= 1000000000000.0;
+            System.out.println("\nAfter removing the element the size of the ArrayList is: " + quakeData.size());  
+        }
+        
         return ret;
     }
 
     public void findClosestQuakes() {
         EarthQuakeParser parser = new EarthQuakeParser();
-        //String source = "data/nov20quakedata.atom";
-        String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        String source = "data/nov20quakedatasmall.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
         ArrayList<QuakeEntry> list  = parser.read(source);
         System.out.println("read data for "+list.size());
 
         Location jakarta  = new Location(-6.211,106.845);
 
-        ArrayList<QuakeEntry> close = getClosest(list,jakarta,10);
+        ArrayList<QuakeEntry> close = getClosest(list,jakarta,3);
         for(int k=0; k < close.size(); k++){
             QuakeEntry entry = close.get(k);
             double distanceInMeters = jakarta.distanceTo(entry.getLocation());
---
+++ Earthquake Searching/SearchingEarthquakeDataStarterProgram/LargestQuakes.java
@@ -0,0 +1,72 @@
+import java.util.*;
+/**
+ * Write a description of class LargestQuakes here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LargestQuakes
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class LargestQuakes
+     */
+    public LargestQuakes()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void findLargestQuakes(){
+        EarthQuakeParser parser = new EarthQuakeParser();
+        String source = "data/nov20quakedatasmall.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        System.out.println("read data for "+list.size());
+        //System.out.println("Largest earthquake is at location "+ indexOfLargest(list) + " and its magnitude is "+ list.get(indexOfLargest(list)).getMagnitude()); 
+        
+        ArrayList<QuakeEntry> answer =  getLargest(list, 5);
+        
+        for (QuakeEntry qe : answer){
+            System.out.println(qe);       
+        }
+        //*/
+        
+        
+        
+    
+    }
+    
+    public ArrayList<QuakeEntry> getLargest(ArrayList<QuakeEntry> quakeData, int howMany){
+        ArrayList<QuakeEntry> ret = new ArrayList<QuakeEntry>();
+        QuakeEntry quake= null; 
+        for(int i=0; i<howMany; i++){
+            ret.add(quakeData.get(indexOfLargest(quakeData)));
+            quakeData.remove(quakeData.get(indexOfLargest(quakeData)));
+            System.out.println("\nAfter removing the element the size of the ArrayList is: " + quakeData.size());  
+        }
+        return ret;
+    
+    } 
+    
+    
+    public int indexOfLargest(ArrayList<QuakeEntry> data){
+        double mag= 0.0;
+        QuakeEntry largest=null;
+        for (QuakeEntry qe : data){
+            if(qe.getMagnitude()> mag){
+            mag = qe.getMagnitude();
+            largest= qe;
+            }       
+        }
+        return data.indexOf(largest);
+    }
+}
