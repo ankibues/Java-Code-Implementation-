5ba11ca9ca4d9fc839df770dd69eef5751a97154
Ankit <ankitbhandariiitr@gmail.com>
2023-04-16 20:22:39 -0400

    WordNGram implement

---
+++ WordNGramStarterProgram/MarkovWordOne.java
@@ -0,0 +1,84 @@
+package WordNGramStarterProgram;
+
+
+/**
+ * Write a description of class MarkovWordOne here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+
+public class MarkovWordOne implements IMarkovModel {
+    private String[] myText;
+    private Random myRandom;
+    
+    public MarkovWordOne() {
+        myRandom = new Random();
+    }
+    
+    public void setRandom(int seed) {
+        myRandom = new Random(seed);
+    }
+    private int indexOf(String[] words, String target, int start){
+        for(int i=start; i< words.length; i++){
+        if(target.equals(words[i])){
+        return i;
+        }    
+        }        
+        return -1;    
+    }
+    public void setTraining(String text){
+		myText = text.split("\\s+");
+	}
+	
+	public String getRandomText(int numWords){
+		StringBuilder sb = new StringBuilder();
+		int index = myRandom.nextInt(myText.length-1);  // random word to start with
+		String key = myText[index];
+		sb.append(key);
+		sb.append(" ");
+		for(int k=0; k < numWords-1; k++){
+		    ArrayList<String> follows = getFollows(key);
+		    //System.out.println("the key is "+ key + "and the words that follows are");
+		    //System.out.println(follows);
+		    if (follows.size() == 0) {
+		        break;
+		    }
+			index = myRandom.nextInt(follows.size());
+			String next = follows.get(index);
+			sb.append(next);
+			sb.append(" ");
+			key = next;
+		}
+		
+		return sb.toString().trim();
+	}
+	
+    private ArrayList<String> getFollows(String key) {
+	    ArrayList<String> follows = new ArrayList<String>();
+	    int pos=0;
+            int ind=0;
+        while(ind !=-1){
+            ind = indexOf(myText,key,pos);
+            if (ind==-1) break;
+            if((ind + 1)> myText.length-1) break;
+            follows.add(myText[ind + 1]);
+            pos = ind+1;
+        }
+        return follows;
+        }
+    
+    public void testIndexOf(){
+    String test= "this is just a test yes this is a simple test";
+    String[] words = test.split("\\s+");
+    System.out.println(indexOf(words,"this",0));
+    System.out.println(indexOf(words,"this",3));
+    System.out.println(indexOf(words,"frog",0));
+    System.out.println(indexOf(words,"frog",5));
+    System.out.println(indexOf(words,"simple",2));
+    System.out.println(indexOf(words,"test",4));
+    }
+
+}
